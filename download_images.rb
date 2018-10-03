83034f2a35abc6d201af7d19639d9929f3d3b716
mperaic <mperaic@gmail.com>
2018-10-03 13:39:50 +0200

    Add method for image download

--- download_images.rb
+++ download_images.rb
@@ -1,7 +1,14 @@
+#!/usr/bin/env ruby
+
 require 'open-uri'
+
+
+urls = File.write('urls.txt', "https://i.postimg.cc/tn7hL1pC/jaz.jpg, https://i.postimg.cc/Z0qF9Qpt/kuruz.jpg, https://i.postimg.cc/62MrjVsm/vrh3.jpg")
 
 def download_image(url, dest)
   open(url) do |u|
     File.open(dest, 'wb') { |f| f.write(u.read) }
   end
 end
+
+urls.each { |url| download_image(url, url.split('/').last) }
