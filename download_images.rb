b21b49b3a59f9776e8c1ab083da086ca567dc40a
mperaic <mperaic@gmail.com>
2018-10-03 15:07:20 +0200

    Download URLs from a file

--- download_images.rb
+++ download_images.rb
@@ -1,15 +1,14 @@
 require 'open-uri'
 
-urls = open('https://github.com/mperaic/image_fetcher/blob/master/urls.txt').readlines
 def download_image(url, dest)
   open(url) do |u|
     File.open(dest, 'wb') { |f| f.write(u.read) }
   end
 end
 
+urls = open('./urls.txt').readlines
 
-urls = File.read('urls.txt')
 
-urls.each do 
-  { |url| download_image(url, url.split('/').last) }
+urls.each do |url| 
+  download_image(url, url.split('/').last) 
 end
